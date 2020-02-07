86b7773a4114d198843f037890318039c94d9d69
henhells <43005450+henhells@users.noreply.github.com>
2020-02-07 14:36:46 +0100

    More code in init

--- Java-kod/Curve25519.java
+++ Java-kod/Curve25519.java
@@ -2,14 +2,14 @@ import java.security.SecureRandom;
 import java.util.ArrayList;
 
 public class Curve25519 {
-	
+
 	final static int KEY_LENGTH = 32;
 	final static int NUMBER_OF_EPHEMERAL_KEYS = 10;
 	final static Sha512 sha512provider = new sha512_provider();
-	
-	
+
+
 	/**
-	 * 
+	 *
 	 * @param length: Length of the desired byte-array
 	 * @return byte-array with random bytes
 	 */
@@ -20,16 +20,15 @@ public class Curve25519 {
 		return result;
 	}
 	/**
-	 * 
+	 *
 	 * @param random: Array of random bytes
 	 * @return "Curve-friendly" byte-array
 	 */
-	
+
 	public byte[] generatePrivateKey(byte[] random) {
 		byte[] privateKey = new byte[KEY_LENGTH];
-		
 		System.arraycopy(random, 0, privateKey, 0, 32);
-		
+
 	    privateKey[0]  &= 248;
 	    privateKey[31] &= 127;
 	    privateKey[31] |= 64;
@@ -37,7 +36,7 @@ public class Curve25519 {
 	    return privateKey;
 	}
 	/**
-	 * 
+	 *
 	 * @param privateKey: "Curve-friendly" byte array (with length 32)
 	 * @return PublicKey that can be used in key-exchanges
 	 */
@@ -47,7 +46,7 @@ public class Curve25519 {
 		return publicKey;
 	}
 	/**
-	 * 
+	 *
 	 * @return A Curve25519 key-pair
 	 */
 	public Curve_KeyPair generateKeyPair() {
@@ -56,46 +55,45 @@ public class Curve25519 {
 		return new Curve_KeyPair(privateKey, publicKey);
 	}
 	/**
-	 * 
+	 *
 	 * @return An array of Curve25519 key-pairs, where the length depends on global variable
 	 */
-	
+
 	public Curve_KeyPair[] generatePreKeys() {
 		Curve_KeyPair[] preKeys= new Curve_KeyPair[NUMBER_OF_EPHEMERAL_KEYS];
 		for(int i = 0; i < preKeys.length; i++) {
-			preKeys[i] = generateKeyPair(); 
+			preKeys[i] = generateKeyPair();
 		}
 		return preKeys;
 	}
 	/**
-	 * 
+	 *
 	 * @return A pre-key-bundle, containing both the private and the public parts of the keys
 	 */
-	
+
 	public preKeyBundle generatePreKeyBundle() {
 		Curve_KeyPair identityKey = generateKeyPair();
 		Curve_KeyPair preKey = generateKeyPair();
 		Curve_KeyPair[] preKeys = generatePreKeys();
-		
+
 		byte[] signedPublicPreKey = new byte[64];
 		curve_sigs.curve25519_sign(sha512provider, signedPublicPreKey, identityKey.getPublicKey(), preKey.getPublicKey(), preKey.getPublicKey().length, getRandom(32));
-		
+
 		ArrayList<byte[]> privatePreKeys = new ArrayList<byte[]>();
 		ArrayList<byte[]> publicPreKeys = new ArrayList<byte[]>();
 		for(int i = 0; i < NUMBER_OF_EPHEMERAL_KEYS; i++) {
 			privatePreKeys.add(preKeys[i].getPrivateKey());
 			publicPreKeys.add(preKeys[i].getPublicKey());
 		}
-		
 		preKeyBundlePrivate privateKeys = new preKeyBundlePrivate(identityKey.getPrivateKey(), preKey.getPrivateKey(), privatePreKeys);
 		preKeyBundlePublic publicKeys = new preKeyBundlePublic(identityKey.getPublicKey(), signedPublicPreKey, preKey.getPublicKey(), publicPreKeys);
 		return new preKeyBundle(privateKeys, publicKeys);
 	}
 	/**
-	 * 
+	 *
 	 * @param ourPrivate: Our privateKey
 	 * @param theirPublic: Their publicKey
-	 * @return a shared agreement 
+	 * @return a shared agreement
 	 */
 	public byte[] calculateAgreement(byte[] ourPrivate, byte[] theirPublic) {
 	    byte[] agreement = new byte[32];
@@ -103,11 +101,22 @@ public class Curve25519 {
 
 	    return agreement;
 	}
-	
+
+	public byte[] appendArray(byte[] array1, byte[] array2) {
+		byte[] result = new byte[array1.length + array2.length];
+		for(int i = 0; i < array1.length; i++) {
+			result[i] = array1[i];
+		}
+		for(int j = 0; j < array2.length; j++) {
+			result[j + array1.length] = array2[j];
+		}
+	}
 
 	public void init(preKeyBundlePrivate ours, preKeyBundlePublic theirs) {
 			byte[] p1 = calculateAgreement(ours.getPrivateIdentityKey(), theirs.getPublicPreKey());
 			byte[] p2 = calculateAgreement(ours.getPrivateOneTimePreKey(0), theirs.getPublicIdentityKey());
-			byte[] p3 = calcualteAgreement(ours.getPrivateOneTimePreKey(0), theirs)
+			byte[] p3 = calcualteAgreement(ours.getPrivateOneTimePreKey(0), theirs.getPublicPreKey());
+			byte[] p4 = calcualteAgreement(ours.getPrivateOneTimePreKey(0), theirs.getPublicOneTimePreKey(0));
+			appendArray(p1, appendArray(p2, appendArray(p3, p4)));
 	}
 }
--- Java-kod/sha512_provider.java
+++ Java-kod/sha512_provider.java
@@ -3,11 +3,11 @@ import java.security.NoSuchAlgorithmException;
 
 public class sha512_provider implements Sha512{
 
-	
+
 	@Override
 	public void calculateDigest(byte[] out, byte[] in, long length) {
 		try {
-			MessageDigest md = MessageDigest.getInstance("SHA-¨512");
+			MessageDigest md = MessageDigest.getInstance("SHA-512");
 			out = md.digest(in);
 		}
 		catch(NoSuchAlgorithmException e) {
