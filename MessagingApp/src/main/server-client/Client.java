2be7aff2301e634a82ec545b52fd875ddac7f31e
John Blåber Kristoffersson <John.kristoffersson@gmail.com>
2020-02-14 13:44:34 +0100

    added PortScanner

--- MessagingApp/src/main/server-client/Client.java
+++ MessagingApp/src/main/server-client/Client.java
@@ -1,66 +1,62 @@
-import java.io.*; 
-import java.net.*; 
-import java.util.Scanner; 
-  
-public class Client  
-{ 
-    final static int ServerPort = 1234; 
-  
-    public static void main(String args[]) throws IOException
-    { 
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class Client {
+    final static int ServerPort = 1234;
+
+    public static void main(String args[]) throws IOException {
         final Scanner scn = new Scanner(System.in);
-          
-        // getting localhost ip 
-        InetAddress ip = InetAddress.getByName("localhost"); 
-          
-        // establish the connection 
-        Socket s = new Socket(ip, ServerPort); 
-          
-        // obtaining input and out streams 
+
+        // getting localhost ip
+        InetAddress ip = InetAddress.getByName("localhost");
+
+        // establish the connection
+        Socket s = new Socket(ip, ServerPort);
+
+        // obtaining input and out streams
         final DataInputStream dis = new DataInputStream(s.getInputStream());
         final DataOutputStream dos = new DataOutputStream(s.getOutputStream());
-  
-        // sendMessage thread 
-        Thread sendMessage = new Thread(new Runnable()  
-        { 
+
+        // sendMessage thread
+        Thread sendMessage = new Thread(new Runnable() {
             @Override
-            public void run() { 
-                while (true) { 
-  
-                    // read the message to deliver. 
-                    String msg = scn.nextLine(); 
-                      
-                    try { 
-                        // write on the output stream 
-                        dos.writeUTF(msg); 
-                    } catch (IOException e) { 
-                        e.printStackTrace(); 
-                    } 
-                } 
-            } 
-        }); 
-          
-        // readMessage thread 
-        Thread readMessage = new Thread(new Runnable()  
-        { 
+            public void run() {
+                while (true) {
+
+                    // read the message to deliver.
+                    String msg = scn.nextLine();
+
+                    try {
+                        // write on the output stream
+                        dos.writeUTF(msg);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        // readMessage thread
+        Thread readMessage = new Thread(new Runnable() {
             @Override
-            public void run() { 
-  
-                while (true) { 
-                    try { 
-                        // read the message sent to this client 
-                        String msg = dis.readUTF(); 
-                        System.out.println(msg); 
-                    } catch (IOException e) { 
-  
-                        e.printStackTrace(); 
-                    } 
-                } 
-            } 
-        }); 
-  
-        sendMessage.start(); 
-        readMessage.start(); 
-  
-    } 
-} 
+            public void run() {
+
+                while (true) {
+                    try {
+                        // read the message sent to this client
+                        String msg = dis.readUTF();
+                        System.out.println(msg);
+                    } catch (IOException e) {
+
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        sendMessage.start();
+        readMessage.start();
+
+    }
+}
