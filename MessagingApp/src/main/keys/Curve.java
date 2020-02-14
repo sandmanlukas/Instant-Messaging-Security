3b52d10cc49b2d2e05987877700efd5b9c11ebf6
Lukas Sandman <lukas.sandman@yahoo.com>
2020-02-14 12:01:22 +0100

    init progress

--- MessagingApp/src/main/keys/Curve.java
+++ MessagingApp/src/main/keys/Curve.java
@@ -1,21 +1,25 @@
 
 
+import org.apache.commons.lang3.tuple.MutableTriple;
 import org.whispersystems.curve25519.Curve25519;
 import org.whispersystems.curve25519.Curve25519KeyPair;
 import org.whispersystems.curve25519.java.curve_sigs;
 import org.whispersystems.curve25519.java.scalarmult;
 import org.whispersystems.curve25519.JCESha512Provider;
 import org.whispersystems.libsignal.ecc.*;
+import org.whispersystems.libsignal.kdf.DerivedMessageSecrets;
+import org.whispersystems.libsignal.kdf.DerivedRootSecrets;
 import org.whispersystems.libsignal.state.PreKeyRecord;
 import org.whispersystems.libsignal.util.KeyHelper;
 import org.whispersystems.libsignal.kdf.HKDF;
 import com.google.common.primitives.Bytes;
+import org.whispersystems.libsignal.util.Pair;
+import org.apache.commons.lang3.tuple.Triple;
+
 
 import java.security.SecureRandom;
 import java.util.ArrayList;
-import java.util.List;
-
-import static org.whispersystems.libsignal.ecc.Curve.generateKeyPair;
+import java.util.Arrays;
 
 
 public class Curve {
@@ -23,6 +27,9 @@ public class Curve {
    final static int KEY_LENGTH = 32;
     final static int NUMBER_OF_EPHEMERAL_KEYS = 10;
     final static JCESha512Provider sha512provider = new JCESha512Provider();
+    static Curve25519 curve = Curve25519.getInstance(Curve25519.BEST);
+    static  HKDF kdf = HKDF.createFor(3);
+
 
 
     /**
@@ -42,16 +49,19 @@ public class Curve {
      * @param privateKey: "Curve-friendly" byte array (with length 32)
      * @return PublicKey that can be used in key-exchanges
      */
+    /*
     public byte[] generatePublicKey(byte[] privateKey) {
         byte[] publicKey = new byte[KEY_LENGTH];
         curve_sigs.curve25519_keygen(publicKey, privateKey);
         return publicKey;
     }
+    */
+
     /**
      *
      * @return An array of Curve25519 key-pairs, where the length depends on global variable
      */
-
+/*
     public ECKeyPair[] generatePreKeys() {
         ECKeyPair[] preKeys= new ECKeyPair[NUMBER_OF_EPHEMERAL_KEYS];
         for(int i = 0; i < preKeys.length; i++) {
@@ -59,36 +69,40 @@ public class Curve {
         }
         return preKeys;
     }
+*/
+    public Pair<ArrayList<byte []>, ArrayList<byte []>> generateEphemeralKeys() {
+        //Curve25519 curve = Curve25519.getInstance(Curve25519.BEST);
+        ArrayList<byte[]> ephemeralPrivateKeys = new ArrayList<byte[]>();
+        ArrayList<byte []> ephemeralPublicKeys = new ArrayList<byte[]>();
+        for (int i = 0; i < NUMBER_OF_EPHEMERAL_KEYS; i++) {
+            Curve25519KeyPair ephemeralKeys = curve.generateKeyPair();
+            ephemeralPrivateKeys.add(ephemeralKeys.getPrivateKey());
+            ephemeralPublicKeys.add(ephemeralKeys.getPublicKey());
 
+        }
+        return new Pair(ephemeralPrivateKeys, ephemeralPublicKeys);
+    }
     /**
      *
      * @return A pre-key-bundle, containing both the private and the public parts of the keys
      */
 
     public preKeyBundle generatePreKeyBundle() {
-        Curve25519 curve = Curve25519.getInstance(Curve25519.BEST);
+        //Curve25519 curve = Curve25519.getInstance(Curve25519.BEST);
         Curve25519KeyPair identityKeyPair = curve.generateKeyPair();
         Curve25519KeyPair preKeyPair = curve.generateKeyPair();
-        //Curve25519KeyPair [] preKeys = new Curve25519KeyPair[NUMBER_OF_EPHEMERAL_KEYS];
-
 
         byte[] signedPublicPreKey = new byte[64];
         curve_sigs.curve25519_sign(sha512provider,
-                signedPublicPreKey, identityKeyPair.getPublicKey(),
-                preKeyPair.getPublicKey(), preKeyPair.getPublicKey().length,
-                getRandom(32));
+                signedPublicPreKey, identityKeyPair.getPrivateKey(),
+                preKeyPair.getPublicKey(), preKeyPair.getPublicKey().length,getRandom(64));
 
-        ArrayList<byte []> privatePreKeys = new ArrayList<byte []>();
-        ArrayList<byte []> publicPreKeys = new ArrayList<byte []>();
-        for(int i = 0; i < NUMBER_OF_EPHEMERAL_KEYS; i++) {
-            Curve25519KeyPair tempPair = curve.generateKeyPair();
-            privatePreKeys.add(tempPair.getPrivateKey());
-            publicPreKeys.add(tempPair.getPublicKey());
-        }
+        Pair <ArrayList<byte []>, ArrayList<byte []>> ephemeralPair = generateEphemeralKeys();
+
         preKeyBundlePrivate privateKeys = new preKeyBundlePrivate(identityKeyPair.getPrivateKey(),
-                preKeyPair.getPrivateKey(), privatePreKeys);
+                preKeyPair.getPrivateKey(), ephemeralPair.first());
         preKeyBundlePublic publicKeys = new preKeyBundlePublic(identityKeyPair.getPublicKey(),
-                signedPublicPreKey, preKeyPair.getPublicKey(), publicPreKeys);
+                signedPublicPreKey, preKeyPair.getPublicKey(), ephemeralPair.second());
         return new preKeyBundle(privateKeys, publicKeys);
     }
 
@@ -100,73 +114,109 @@ public class Curve {
      * @return a shared agreement
      */
     public byte[] calculateAgreement(byte[] ourPrivate, byte[] theirPublic) {
+
         if (ourPrivate == null || theirPublic == null) {
             throw new IllegalArgumentException("Keys cannot be null!");
         }
-        if (ourPrivate.length != 32 || theirPublic.length != 32 ){
-            throw  new IllegalArgumentException("Keys must be 32 bytes long");
+        if (ourPrivate.length != 32 ){
+            throw  new IllegalArgumentException("Private key must be 32 bytes long");
         }
+
+
         byte[] agreement = new byte[32];
         scalarmult.crypto_scalarmult(agreement, ourPrivate, theirPublic);
 
         return agreement;
     }
-/*
-    public byte[] appendArray(byte[] array1, byte[] array2) {
-        byte[] result = new byte[array1.length + array2.length];
-        for(int i = 0; i < array1.length; i++) {
-            result[i] = array1[i];
-        }
-        for(int j = 0; j < array2.length; j++) {
-            result[j + array1.length] = array2[j];
-        }
-        return result;
+    public void initBob (byte [] ephemeralAlice, byte [] ratchetAlice, ArrayList<byte []> preKeysAlice, preKeyBundlePrivate ours, preKeyBundlePublic bundleAlice){
+        byte [] info = new byte [0];
+
+        byte [] p1 = calculateAgreement(ours.getPrivatePreKey(), bundleAlice.getPublicIdentityKey());
+        byte [] p2 = calculateAgreement(ephemeralAlice, ours.getPrivateIdentityKey());
+        byte [] p3 = calculateAgreement(ephemeralAlice, ours.getPrivatePreKey());
+        byte [] p4 = calculateAgreement(ephemeralAlice, preKeysAlice.get(0));
+        byte [] result = Bytes.concat(p1, p2, p3, p4);
+
+        byte [] secret = kdf.deriveSecrets(result, info, 64);
+
+        DerivedRootSecrets rootSecrets = new DerivedRootSecrets(secret);
+        byte [] root1 = rootSecrets.getRootKey();
+
+        byte [] p5 = calculateAgreement(ours.getPrivatePreKey(), ratchetAlice);
+        byte [] result2 = Bytes.concat(p5, root1);
+
+        byte [] secret2 = kdf.deriveSecrets(result2, info, 64);
+        DerivedRootSecrets rootSecrets2 = new DerivedRootSecrets(secret2);
+
+        byte [] root2 = rootSecrets2.getRootKey();
+        byte [] chain1 = rootSecrets2.getChainKey();
+
+        byte [] secret3 = kdf.deriveSecrets(chain1, info, 64);
+
+        DerivedRootSecrets rootSecrets3 = new DerivedRootSecrets(secret3);
+
+        byte [] message = rootSecrets3.getRootKey();
+        byte [] realChain = rootSecrets3.getChainKey();
+
+
+
     }
-*/
-    public void init(preKeyBundlePrivate ours, preKeyBundlePublic theirs) {
+
+
+
+    public Triple<byte [], byte [], ArrayList<byte []>> initAlice(preKeyBundlePrivate ours, preKeyBundlePublic theirs){
+        byte [] info = new byte [0];
+        Curve25519KeyPair ephemeralKeyPair = curve.generateKeyPair();
+        Curve25519KeyPair ratchetKeyPair = curve.generateKeyPair();
+
+        byte [] ephemeralPrivate = ephemeralKeyPair.getPrivateKey();
+
         byte[] p1 = calculateAgreement(ours.getPrivateIdentityKey(), theirs.getPublicPreKey());
-        byte[] p2 = calculateAgreement(ours.getPrivateOneTimePreKey(0), theirs.getPublicIdentityKey());
-        byte[] p3 = calculateAgreement(ours.getPrivateOneTimePreKey(0), theirs.getPublicPreKey());
-        byte[] p4 = calculateAgreement(ours.getPrivateOneTimePreKey(0), theirs.getPublicOneTimePreKey(0));
-        //byte[] result = appendArray(p1, appendArray(p2, appendArray(p3, p4)));
+        byte[] p2 = calculateAgreement(ephemeralPrivate, theirs.getPublicIdentityKey());
+        byte[] p3 = calculateAgreement(ephemeralPrivate, theirs.getPublicPreKey());
+        byte[] p4 = calculateAgreement(ephemeralPrivate, theirs.getPublicOneTimePreKey(0));
+
         byte [] result = Bytes.concat(p1,p2,p3,p4);
+        byte [] secret = kdf.deriveSecrets(result,info, 64 );
+
+        DerivedRootSecrets rootSecrets = new DerivedRootSecrets(secret);
+        byte [] root1 = rootSecrets.getRootKey();
+        byte [] p5 = calculateAgreement(ratchetKeyPair.getPrivateKey(), theirs.getPublicPreKey());
+        byte [] result2 = Bytes.concat(p5, root1);
+        byte [] secret2 = kdf.deriveSecrets(result2, info, 64);
+
+        DerivedRootSecrets rootSecrets2 = new DerivedRootSecrets(secret2);
+        byte [] chain = rootSecrets2.getChainKey();
+        byte [] secret3 = kdf.deriveSecrets(chain, info, 64);
+
+        DerivedRootSecrets rootSecrets3 = new DerivedRootSecrets(secret3);
+        byte [] message = rootSecrets3.getRootKey();
+        byte [] realChain = rootSecrets3.getChainKey();
+
+        // Remove public ephemeralKey that was used.
+        theirs.removePublicOneTimePreKey(0);
+        byte [] ephemeralPublic = ephemeralKeyPair.getPublicKey();
+        byte [] ratchetPublic = ratchetKeyPair.getPublicKey();
+
+        return new MutableTriple<byte [], byte[], ArrayList<byte[]>>(ephemeralPublic, ratchetPublic, theirs.getPublicOneTimePreKeys());
+
     }
 
     public static void main(String[] args) {
-        Curve curve = new Curve();
+        Curve curveClass = new Curve();
 
-        Curve25519KeyPair keyPairAlice = Curve25519.getInstance(Curve25519.BEST).generateKeyPair();
+        Curve25519KeyPair keyPairAlice = curve.generateKeyPair();
         byte [] privateKeyAlice = keyPairAlice.getPrivateKey();
         byte [] publicKeyAlice = keyPairAlice.getPublicKey();
 
-        Curve25519KeyPair keyPairBob = Curve25519.getInstance(Curve25519.BEST).generateKeyPair();
+        Curve25519KeyPair keyPairBob = curve.generateKeyPair();
         byte [] privateKeyBob = keyPairBob.getPrivateKey();
         byte [] publicKeyBob = keyPairBob.getPublicKey();
 
-        //byte [] sharedSecret = curve.calculateAgreement(privateKey,publicKey);
+        preKeyBundle preKeysAlice = curveClass.generatePreKeyBundle();
+        preKeyBundle preKeysBob = curveClass.generatePreKeyBundle();
 
-
-       // List<PreKeyRecord> preKeysAlice = KeyHelper.generatePreKeys(1,10);
-        //List<PreKeyRecord> preKeysBob = KeyHelper.generatePreKeys(1,10);
-
-        preKeyBundle preKeysAlice = curve.generatePreKeyBundle();
-        preKeyBundle preKeysBob = curve.generatePreKeyBundle();
-
-
-        curve.init(preKeysAlice.getPrivateKeys(), preKeysBob.getPublicKeys());
-
-        //Curve_KeyPair keyPair = new Curve_KeyPair(privateKey, publicKey);
-        //System.out.println(keyPair.getPublicKey() + ", " + keyPair.getPrivateKey());
-
-
-
-
-        //preKeyBundle preKeys = new preKeyBundle();
-        //preKeyBundle preKeys = generatePreKeyBundle();
-
-
-
-
+        Triple<byte [], byte [], ArrayList<byte []>> initiation = curveClass.initAlice(preKeysAlice.getPrivateKeys(), preKeysBob.getPublicKeys());
 
     }
 }
