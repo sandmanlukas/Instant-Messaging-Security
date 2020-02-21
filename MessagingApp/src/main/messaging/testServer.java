05058db75904a3495733bd77eac5f00b0290d840
henhells <43005450+henhells@users.noreply.github.com>
2020-02-21 08:48:28 +0100

    commit for merge

---
+++ MessagingApp/src/main/messaging/testServer.java
@@ -0,0 +1,24 @@
+import javafx.util.Pair;
+import java.util.HashMap;
+
+public class testServer {
+
+    private final int ip;
+    private int activeUsers;
+    public HashMap<String, Pair<preKeyBundlePublic,Integer>> clients;
+
+    public testServer(int ip) {
+        this.ip = ip;
+        activeUsers = 0;
+        HashMap<String, Pair<preKeyBundlePublic, Integer>> clients = new HashMap<>();
+    }
+
+    public void addClient(String client, preKeyBundlePublic preKeys, int ip) {
+        clients.put(client, new Pair(preKeys, ip));
+    }
+
+    public Pair<preKeyBundlePublic, Integer> getClient(String client) {
+        return clients.get(client);
+    }
+
+}
