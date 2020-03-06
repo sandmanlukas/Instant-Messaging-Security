c9a0a13f1a949ea4ac6b9775bf48f5953438d359
henhells <43005450+henhells@users.noreply.github.com>
2020-03-06 13:43:28 +0100

    jadu

---
+++ MessagingApp/src/main/sql/init.sql
@@ -0,0 +1,9 @@
+\set QUIET true
+SET client_min_messages TO WARNING; -- Less talk please.
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
+GRANT ALL ON SCHEMA public TO postgres;
+\set QUIET false
+
+\i tables.sql
+\i trigger.sql
