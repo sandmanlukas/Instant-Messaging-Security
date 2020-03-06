45071416e32976b4e7165f258655ccadfa263721
David Brandberg <brdavid@student.chalmers.se>
2020-03-06 09:02:06 +0100

    Changed som stuff in mainchat

--- MessagingApp/src/frontend/GUI/GUIMain.java
+++ MessagingApp/src/frontend/GUI/GUIMain.java
@@ -8,6 +8,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 
@@ -86,18 +87,72 @@ public class GUIMain extends Application {
 
 
         //trying to add text field to write messages at the bottom of the left pane.....
+
+        HBox lowerHBox = new HBox();
+        VBox textDispVBox =new VBox();
+        VBox chatDisplay = new VBox();
+
+
+        TextField writeMessage = new TextField();
+        writeMessage.setMaxSize(500,100);
+        writeMessage.setPromptText("Write message here");
+        //conversationGrid.setBottom(writeMessage);
+
+        Button sendButton = new Button("Send");
+        sendButton.setOnAction(e -> {
+            //System.out.println(writeMessage.getText());
+            Text sent = new Text("> " + writeMessage.getText());
+            textDispVBox.getChildren().add(sent);
+            writeMessage.clear();
+        });
+        lowerHBox.setHgrow(writeMessage, Priority.ALWAYS);//Added this line
+        lowerHBox.setHgrow(sendButton, Priority.ALWAYS);//Added this line
+        lowerHBox.getChildren().addAll(writeMessage,sendButton);
+        conversationGrid.getChildren().add(textDispVBox);
+
+        BorderPane border = new BorderPane();
+        border.setRight(chattGrid);
+
+        border.setBottom(lowerHBox);
+        border.setLeft(textDispVBox);
+        //border.setLeft(conversationGrid);
+        //border.setBottom(writeMessage);
+        //border.setCenter(sendButton);
+
+        primaryStage.setScene((new Scene(border, 700, 500)));
+        primaryStage.show();
+
+
+
+
+    }
+}
+
+    /*public void mainChatPage(Stage primaryStage){
+    //list of active chats, or where to create new conversations
+    GridPane chattGrid = new GridPane();
+        chattGrid.setScaleX(200);
+                chattGrid.setStyle("-fx-border-color: blue;");
+
+                //shows conversations one at the time
+                BorderPane conversationGrid = new BorderPane();
+                conversationGrid.setScaleX(500);
+                conversationGrid.setStyle("-fx-border-color: #A9A9A9;");
+
+
+                //trying to add text field to write messages at the bottom of the left pane.....
         /*
         TextField writeMessage = new TextField();
         writeMessage.setMaxSize(500,10);
         writeMessage.setText("Write message here");
         conversationGrid.setBottom(writeMessage);
         */
-        BorderPane border = new BorderPane();
-        border.setRight(chattGrid);
-        border.setLeft(conversationGrid);
+               /* BorderPane border = new BorderPane();
+                border.setRight(chattGrid);
+                border.setLeft(conversationGrid);
 
-        primaryStage.setScene((new Scene(border, 700, 500)));
-        primaryStage.show();
+                primaryStage.setScene((new Scene(border, 700, 500)));
+                primaryStage.show();
 
-    }
-}
+                }
+                }*/
